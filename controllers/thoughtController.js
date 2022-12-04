c56be65d2344e6afc74fe02106db90af61fcca2c
Matthew Dacanay <mattdack@uw.edu>
2022-12-04 14:30:29 -0600

    Finalizes the functionality to add and remove reactions

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -96,7 +96,7 @@ module.exports = {
   removeReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.id },
-      { $addToSet: { reactions: { reactionId: req.params.reactionId } } },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
       { runValidators: true, new: true }
     )
       .then((thought) =>
